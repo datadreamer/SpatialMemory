08f63db9d07e9a67e100078d0d09a803bcede38d
Aaron Siegel <datadreamerlabs@gmail.com>
2015-04-02 17:49:53 -0700

    Removed unused methods from example.

--- app/src/main/java/com/datadreamer/spatialmemory/GeofenceTransitionsIntentService.java
+++ app/src/main/java/com/datadreamer/spatialmemory/GeofenceTransitionsIntentService.java
@@ -7,19 +7,14 @@ import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
-import android.text.TextUtils;
 import android.util.Log;
 
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -58,9 +53,6 @@ public class GeofenceTransitionsIntentService extends IntentService implements H
         int geofenceTransition = geofencingEvent.getGeofenceTransition();
         if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER || geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
             List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
-            //String geofenceTransitionDetails = getGeofenceTransitionDetails(this, geofenceTransition, triggeringGeofences);
-            //sendNotification(geofenceTransitionDetails);
-            //Log.i(TAG, geofenceTransitionDetails);
             for (Geofence geofence : triggeringGeofences) {
                 Log.d(TAG, geofence.getRequestId());
                 id = geofence.getRequestId();
@@ -73,68 +65,9 @@ public class GeofenceTransitionsIntentService extends IntentService implements H
             // grab data from server since this isn't working
             dataTask = new HttpTask(this);
             dataTask.execute(apiUrl + "?action=info&id=" + id);
-            // TODO: issue notification with thumb and
-            // photo data
         } else {
             // Log the error.
             Log.e(TAG, getString(R.string.geofence_transition_invalid_type, geofenceTransition));
-        }
-    }
-
-    /**
-     * Posts a notification in the notification bar when a transition is detected.
-     * If the user clicks the notification, control goes to the PhotoActivity.
-     */
-    private void sendNotification(String notificationDetails) {
-        //Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);    // Create an explicit content Intent that starts the main Activity.
-        Intent notificationIntent = new Intent(getApplicationContext(), PhotoActivity.class);
-        //notificationIntent.putExtra("photo", )
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);  // Construct a task stack.
-        stackBuilder.addParentStack(MainActivity.class);                // Add the main Activity to the task stack as the parent.
-        stackBuilder.addNextIntent(notificationIntent);                 // Push the content Intent onto the stack.
-
-        // Get a PendingIntent containing the entire back stack.
-        PendingIntent notificationPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-        // Get a notification builder that's compatible with platform versions >= 4
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-
-        // Define the notification settings.
-        // TODO: use image that was just downloaded for icon.
-        // TODO: grab title and date of image and display in notification.
-        builder.setSmallIcon(R.drawable.ic_launcher)
-                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher))
-                .setColor(Color.RED)
-                .setContentTitle(notificationDetails)
-                .setContentText(getString(R.string.geofence_transition_notification_text))
-                .setContentIntent(notificationPendingIntent);
-
-        // Dismiss notification once the user touches it.
-        builder.setAutoCancel(true);
-        // Get an instance of the Notification manager
-        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        // Issue the notification
-        mNotificationManager.notify(0, builder.build());
-    }
-
-
-    private String getGeofenceTransitionDetails(Context context, int geofenceTransition, List<Geofence> triggeringGeofences) {
-        String geofenceTransitionString = getTransitionString(geofenceTransition);
-        ArrayList triggeringGeofencesIdsList = new ArrayList();
-        for (Geofence geofence : triggeringGeofences) {
-            triggeringGeofencesIdsList.add(geofence.getRequestId());
-        }
-        String triggeringGeofencesIdsString = TextUtils.join(", ", triggeringGeofencesIdsList);
-        return geofenceTransitionString + ": " + triggeringGeofencesIdsString;
-    }
-
-    private String getTransitionString(int transitionType) {
-        switch (transitionType) {
-            case Geofence.GEOFENCE_TRANSITION_ENTER:
-                return getString(R.string.geofence_transition_entered);
-            case Geofence.GEOFENCE_TRANSITION_EXIT:
-                return getString(R.string.geofence_transition_exited);
-            default:
-                return getString(R.string.unknown_geofence_transition);
         }
     }
 
@@ -171,8 +104,8 @@ public class GeofenceTransitionsIntentService extends IntentService implements H
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
         builder.setSmallIcon(R.drawable.ic_launcher)
                 .setLargeIcon(img)
-                .setContentTitle(title)
-                .setContentText(circa)
+                .setContentTitle("Spatial Memory")
+                .setContentText(title +" ("+circa+")")
                 .setContentIntent(notificationPendingIntent);
         builder.setAutoCancel(true);
         NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
